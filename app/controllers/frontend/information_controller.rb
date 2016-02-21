26a34edf2d88cbbc6d6eafdb056df52c8696b199
menghuanwd <651019063@qq.com>
2016-02-21 13:03:28 +0800

    视频

--- app/controllers/frontend/information_controller.rb
+++ app/controllers/frontend/information_controller.rb
@@ -44,44 +44,12 @@ class Frontend::InformationController < FrontendController
       end
       info_sql = top_infors + " union " + select_infos + " order by str_to_date(created_at,'%Y-%m-%d %H:%i:%s') desc limit #{pageset} offset #{((params[:page] || 1).to_i - 1) * pageset})"
 
-    
-
-
-      # Information.where().order("created_at desc").page(params[:page]).per(per)
       it.latest_informations = 
         Rails.cache.fetch("#{it.name}/#{it.id}/information_cache", expires_in: 10.minutes) do
           Information.unscope(:where).find_by_sql(info_sql)
         end
-        
-
-      video_category2 = case it.name
-      when '头条'
-        video_category = VideoCategory.where(name: '资讯').first
-      when '养生'
-        video_category = VideoCategory.where(name: '养生').first
-      when '长寿'
-        video_category = VideoCategory.where(name: '老年').first
-      when '健康管理'
-        video_category = VideoCategory.where(name: '常识').first
-      when '天天护理'
-        video_category = VideoCategory.where(name: '美容').first
-      when '寻医'
-        video_category = VideoCategory.where(name: '医疗').first
-      when '母婴'
-        video_category = VideoCategory.where(name: '儿科').first
-      when '两性健康'
-        video_category = VideoCategory.where(name: '两性').first
-      when '健身减肥'
-        video_category = VideoCategory.where(name: '健身').first
-      else
-        video_category = ''
-      end
-
-      top_video = if video_category.present?
-        Video.select('id, album_name, pic_url, tv_id, time_length, html5_play_url, create_time, video_category_id').where(video_category_id: video_category.id).order("create_time desc").limit(2)
-      else
-        {}
-      end
+      
+      @video_categories = VideoCategory.all
 
       types_image = case it.name
       when '头条'
@@ -95,8 +63,6 @@ class Frontend::InformationController < FrontendController
         name: it.name,
         informations: it.latest_informations,
         sub_types: it.children_items,
-        top_videos: top_video,
-        video_category: video_category2,
         types_images: types_image
       }
     end
